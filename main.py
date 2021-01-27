9f55344a6d74ad381090008aa3c9695278c3980e
Jsss <39948874+Jsssssssss@users.noreply.github.com>
2021-01-28 03:02:11 +0800

    bug fixes

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from controller import *
 
 
 def view():
+    
     # fig, ax = plt.subplots()
     fig, ax = plt.subplots()
+    
     # axes style
     ax.set_title('B-Spline-Curves')
     ax.set_xlim(1, 10)
@@ -24,18 +26,22 @@ def view():
                        "Hide Line: h / H\nExit: q / Q\n",
              fontfamily='serif', fontstyle='oblique', fontsize='medium', color='#000080'
              )
+    
     # add new line button, input textbox and radio button
     addlinebtn = fig.add_axes([0.75, 0.22, 0.2, 0.075])
     inputbox = fig.add_axes([0.85, 0.12, 0.1, 0.07])
     rax = fig.add_axes([0.75, 0.32, 0.2, 0.12])
+    
     # button click
     btncallback = CreatLine(ax)
     binsert = Button(addlinebtn, 'New Line', color='#00BFFF')
     binsert.on_clicked(btncallback.createnewline)
+    
     # input submit
     input_text = TextBox(inputbox, "Degree : ")
     input_text.on_submit(change_degree)
     input_text.set_val("3")
+    
     # radio callback
     radio = RadioButtons(rax, ('Uniform', 'Clamped'))
     radio.on_clicked(change_type)
